e667e1aa172c5824e8331fdd6db8a6b5b7f65a97
hpoe <hunter@hunterpoe.com>
2021-03-22 20:33:38 -0600

    Updates doom config

--- doom-emacs-config/packages.el
+++ doom-emacs-config/packages.el
@@ -22,6 +22,7 @@
 (package! elpy)
 (package! powershell)
 (package! lsp-java)
+(package! wrap-region)
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
